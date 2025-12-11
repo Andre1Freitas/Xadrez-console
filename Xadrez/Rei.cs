c4960226d6b1ba99ef80203b433596eb12fca36c
Andre <andrefreitas09876@gmail.com>
2025-12-11 19:39:00 -0300

    Colocando pecas no tabuleiro

---
+++ Xadrez/Rei.cs
@@ -0,0 +1,16 @@
+﻿using Xadrez_console.tabuleiro;
+
+namespace Xadrez_console.Xadrez
+{
+    class Rei : Peca
+    {
+        public Rei(Cor cor, Tabuleiro tab) : base(cor, tab)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "R ";
+        }
+    }
+}
---
+++ Xadrez/Torre.cs
@@ -0,0 +1,16 @@
+﻿using Xadrez_console.tabuleiro;
+
+namespace Xadrez_console.Xadrez
+{
+    class Torre : Peca
+    {
+        public Torre(Cor cor, Tabuleiro tab) : base(cor, tab)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "T ";
+        }
+    }
+}

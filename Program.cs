5cad78dd3cb7567d85f15e786c0ea16e3f9c3233
Andre <andrefreitas09876@gmail.com>
2025-12-14 13:18:49 -0300

    Melhorando impressao tabuleiro

--- Program.cs
+++ Program.cs
@@ -7,27 +7,20 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
-            /*try
+            try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
                 tab.ColocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 0));
+                tab.ColocarPeca(new Rei(Cor.Branca, tab), new Posicao(0, 2));
 
                 Tela.imprimirTabuleiro(tab);
             }
             catch (TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
-            }*/
-
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
-
-            Console.WriteLine();
+            }
         }
     }
 }
